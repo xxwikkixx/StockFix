d256472b21d383a5e2ec9b8b957cf85058cd4dd2
Waqas <wikkii@msn.com>
2016-05-18 18:01:16 -0400

    sentimental analysis nightmare js scraping

--- StockFix-SentiAnalysis/sentiment.js
+++ StockFix-SentiAnalysis/sentiment.js
@@ -1,5 +1,7 @@
 var sentiment = require('sentiment');
 var sentimental = require('Sentimental')
+var Nightmare = require('nightmare')
+
 //sentiment library
 //https://github.com/thisandagain/sentiment
 var r1 = sentiment('MGT and its subsidiaries are principally engaged in the business of acquiring, developing and monetizing intellectual property assets. MGTs portfolio currently includes social casino and gaming platforms, and ownership stakes in DraftDay.com, a top daily fantasy sports wagering platform and DraftDay Fantasy Sports, Inc. operator of an online entertainment marketing and rewards platform. MGT also recently announced the acquisition of certain technology and assets from D-Vasive Inc., a provider of leading edge anti-spy software.  In conjunction with the acquisition, The Company also announced the proposed appointment of John McAfee as Executive Chairman and Chief Executive Officer.  Further, MGT Capital also intends to change its corporate name to John McAfee Global Technologies, Inc. Closing of the acquisition is contingent on customary conditions including approval by MGTs stockholders.')
