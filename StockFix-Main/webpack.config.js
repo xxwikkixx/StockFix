5491b8c8c8342d966db7de77aaeeb58e1d756875
Jimmy Klein <jimmy.klein4@gmail.com>
2016-06-07 19:05:24 -0400

    Created multiple entrypoint webpack

--- StockFix-Main/webpack.config.js
+++ StockFix-Main/webpack.config.js
@@ -1,8 +1,15 @@
-module.exports = {
-  entry:'./views/components/profile/profileMain.jsx',
-  output:{
-    filename: './public/js/bundles/profileBundle.js'
+var path = require('path');
 
+module.exports = {
+  entry:{
+    main: './views/components/home/home.jsx',
+    signals: './views/components/signals/signals.jsx',
+    profile: './views/components/profile/profileMain.jsx',
+    chat: './views/components/chat/chatMain.jsx'
+  },
+  output:{
+    path: path.join(__dirname, 'public/js/bundles'),
+    filename: '[name].bundle.js'
   },
   module:{
     loaders:[
